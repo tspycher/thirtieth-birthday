f30398874f0b9a8a85457f21eb7b6380e45bd88b
Thomas Spycher <me@tspycher.com>
2014-02-11 20:56:18 +0100

    some styling

--- src/Tspycher/Bundle/ThirtiethBirthdayBundle/Resources/public/js/angular/controller.js
+++ src/Tspycher/Bundle/ThirtiethBirthdayBundle/Resources/public/js/angular/controller.js
@@ -111,6 +111,10 @@ function ParticipantController($scope, $http) {
             });
 
     }
+
+    $scope.gotoCertificate = function () {
+        window.location = '/certificate/'+$scope.login.token;
+    }
     $scope.getParicipantCount();
     $scope.loadGifts();
     $scope.editmode(false);
